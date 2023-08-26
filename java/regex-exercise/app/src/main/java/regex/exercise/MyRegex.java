1820df4d408b8022db35bd32e0a9bcdc69825456
André Ferreira <andresouzafe@gmail.com>
2023-08-26 20:10:41 -0300

    java: add regex challenge

---
+++ java/regex-exercise/app/src/main/java/regex/exercise/MyRegex.java
@@ -0,0 +1,10 @@
+package regex.exercise; 
+
+class MyRegex {
+    private String regex = "[0-5]{0,255}\\.[0-5]{0,255}\\.[0-5]{0,255}\\.[0-5]{0,255}";
+
+    public String getRegex() {
+        return regex;
+    }
+    
+}
